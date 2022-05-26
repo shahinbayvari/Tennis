c4b992a0256694dfea0201a2b75b09aff1f70512
shahinbayvari <shahinbayvari@gmail.com>
2022-05-26 14:30:45 -0400

    Add frame

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        JFrame frame = new JFrame();
+        frame.setResizable(false);
+        frame.setBackground(Color.BLACK);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
     }
 }
