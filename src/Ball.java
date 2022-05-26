7aec80094a046cd88d0c18a127817cd0e09bc735
shahinbayvari <shahinbayvari@gmail.com>
2022-05-26 16:19:21 -0400

    Ball initial speed

--- src/Ball.java
+++ src/Ball.java
@@ -7,6 +7,7 @@ public class Ball extends Rectangle {
     Random random;
     int xVelocity;
     int yVelocity;
+    int initialSpeed = 2;
 
     public Ball(int x, int y, int width, int height) {
         super(x, y, width, height);
