abd0e3091127f140eb84df67d1d92b073a1326a7
shahinbayvari <shahinbayvari@gmail.com>
2022-05-26 15:39:49 -0400

    Finish GameFrame

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -4,8 +4,16 @@ import java.util.*;
 import javax.swing.*;
 
 public class GameFrame extends JFrame {
-    GamePanel panel = new GamePanel();
+    GamePanel panel;
     public GameFrame() {
-
+        panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("Tennis Game");
+        this.setResizable(false);
+        this.setBackground(Color.BLACK);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
     }
 }
