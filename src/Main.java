4694dcdaee8a70907cc53fa6a60ac88ee2498a5a
shahinbayvari <shahinbayvari@gmail.com>
2022-05-26 14:32:42 -0400

    Add board

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
+        Board board = new Board();
         JFrame frame = new JFrame();
+        frame.add(board);
         frame.setResizable(false);
         frame.setBackground(Color.BLACK);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
