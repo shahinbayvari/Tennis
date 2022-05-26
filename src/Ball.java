6e0d014269f0cede7c17d87c606272a195ce62be
shahinbayvari <shahinbayvari@gmail.com>
2022-05-26 16:12:14 -0400

    Random velocity Ball

--- src/Ball.java
+++ src/Ball.java
@@ -9,15 +9,27 @@ public class Ball extends Rectangle {
     int yVelocity;
 
     public Ball(int x, int y, int width, int height) {
+        super(x, y, width, height);
+        random = new Random();
+        int randomXDirection = random.nextInt(2);
+        if (randomXDirection == 0) {
+            randomXDirection--;
+        }
+        setXDirection(randomXDirection);
 
+        int randomYDirection = random.nextInt(2);
+        if (randomYDirection == 0) {
+            randomYDirection--;
+        }
+        setYDirection(randomYDirection);
     }
 
     public void setXDirection(int randomXDirection) {
-
+        xVelocity = randomXDirection;
     }
 
     public void setYDirection(int randomYDirection) {
-
+        yVelocity = randomYDirection;
     }
 
     public void move() {
