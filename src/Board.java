94cbc34cb174038423c5135a37fb31697a74a369
shahinbayvari <shahinbayvari@gmail.com>
2022-05-26 14:43:21 -0400

    Add paddle

--- src/Board.java
+++ src/Board.java
@@ -5,9 +5,17 @@ public class Board extends JPanel {
     private static final int SCREEN_WIDTH = 1000;
     private static final int SCREEN_HEIGHT = 1000;
     private static final Dimension SCREEN_SIZE = new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT);
+    private static final int PADDLE_WIDTH = 100;
+    private static final int PADDLE_HEIGHT = 25;
+    public Paddle paddle;
 
     public Board() {
+        newPaddle();
         this.setPreferredSize(SCREEN_SIZE);
         this.setBackground(Color.BLACK);
+    }
+
+    public void newPaddle() {
+        paddle = new Paddle((SCREEN_WIDTH - PADDLE_WIDTH) / 2, SCREEN_HEIGHT, PADDLE_WIDTH, PADDLE_HEIGHT);
     }
 }
---
+++ src/Paddle.java
@@ -0,0 +1,7 @@
+import java.awt.*;
+
+public class Paddle extends Rectangle {
+    public Paddle(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+}
