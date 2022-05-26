ba2175c995ba3fbf0d96859d3f86c6376721bf63
shahinbayvari <shahinbayvari@gmail.com>
2022-05-26 14:36:12 -0400

    set board size

--- src/Board.java
+++ src/Board.java
@@ -1,7 +1,12 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Board extends JPanel {
-    public Board() {
+    private static final int SCREEN_WIDTH = 1000;
+    private static final int SCREEN_HEIGHT = 1000;
+    private static final Dimension SCREEN_SIZE = new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT);
 
+    public Board() {
+        this.setPreferredSize(SCREEN_SIZE);
     }
 }
